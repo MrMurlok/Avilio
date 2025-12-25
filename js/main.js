76d0cef616ff056c2e6ee1d5bda60d9e86a42a95
MrMurlok <djmurlok.sf@gmail.com>
2025-12-25 20:30:26 +0700

    desktop version

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function loadVideo() {
+    var container = document.querySelector('.video_right');
+    container.innerHTML = '<iframe width="896" height="516" src="https://www.youtube.com/embed/u76NB4no79c?si=xySMChTU3JGIbhjr&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
